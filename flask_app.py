fc7c0833aa67271e3f7d1e328e36131f0cc0c3c2
Rob <robkortekaas@hotmail.co>
2025-06-02 17:46:56 +0200

    basic Flask code

--- flask_app.py
+++ flask_app.py
@@ -4,4 +4,4 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "<p>Er word een branch met de naam main aangemaakt git switcht direct naar die branch</p>"
+    return "<p>antwoord op vraag 4 Er word een branch met de naam main aangemaakt git switcht direct naar die branch</p>"
